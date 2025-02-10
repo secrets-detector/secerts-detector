d2bef46c5aaa45c1feb785495f2a533f56768274
Sean Corkum <seancorkum@Karis-MacBook-Pro.local>
2025-02-09 18:03:11 -0600

    starting to add PG database and grafana for dashboarding

--- test/test_secrets.sh
+++ test/test_secrets.sh
@@ -47,9 +47,6 @@ test_secret "GitHub Token" "ghp_abcdefghijklmnopqrstuvwxyz0123" "github_token"
 # JWT Token
 test_secret "JWT Token" "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.dozjgNryP4J3jVmNHl0w5N_XgL0n3I9PlFUP0THsR8U" "jwt_token"
 
-# Google API Key
-test_secret "Google API Key" "AIzaSyDCvp5MTJLUdtBYEKYWXJrlLju3JgdqXqw" "google_api"
-
 # Test false positive prevention
 test_secret "Non-Secret" "ThisIsNotASecret123" "No secrets detected"
 
