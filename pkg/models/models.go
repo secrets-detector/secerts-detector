b4e093d244713e8303e30f0a620186f267286ef3
Sean Corkum <seancorkum81@gmail.com>
2025-03-01 10:29:50 -0600

    update package models.go

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,7 +5,9 @@ type SecretFinding struct {
 	Value    string `json:"value"`
 	StartPos int    `json:"start_pos"`
 	EndPos   int    `json:"end_pos"`
-	FilePath string `json:"file_path"`
+	FilePath string `json:"file_path,omitempty"`
+	IsValid  bool   `json:"is_valid"`
+	Message  string `json:"message,omitempty"`
 }
 
 type ValidationRequest struct {
@@ -15,4 +17,9 @@ type ValidationRequest struct {
 type ValidationResponse struct {
 	IsValid bool   `json:"is_valid"`
 	Message string `json:"message"`
+}
+
+type DetectionResponse struct {
+	Findings []SecretFinding `json:"findings"`
+	Message  string          `json:"message"`
 }
